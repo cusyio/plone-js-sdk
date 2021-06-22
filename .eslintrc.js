ef6acc66a7a647e8bee8df02ddfc992ce2d1170e
Thomas Massmann <thomas.massmann@it-spir.it>
2021-06-22 09:58:04 +0200

    chore: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,18 @@
 module.exports = {
   parser: 'babel-eslint',
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module',
+  },
   extends: ['prettier'],
   plugins: ['prettier'],
   env: {
     mocha: true,
-    node: true
+    node: true,
   },
   rules: {
     strict: 0,
     'comma-dangle': 0,
-    'prettier/prettier': 'error'
-  }
+    'prettier/prettier': 'error',
+  },
 };
