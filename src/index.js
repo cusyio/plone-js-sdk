48d820621132a1a84b1934f3962722119cbe8b2b
Thomas Massmann <thomas.massmann@it-spir.it>
2021-10-15 16:23:20 -0400

    feat: :sparkles: add retry and caching adapter for the axios client

--- src/index.js
+++ src/index.js
@@ -1,5 +1,66 @@
 import axios from 'axios';
+import axiosRetry from 'axios-retry';
+import {
+  cacheAdapterEnhancer,
+  throttleAdapterEnhancer,
+} from 'axios-extensions';
 import { joinURL, normalizeURL, withQuery } from 'ufo';
+import LRUCache from 'lru-cache';
+
+/**
+ * Cache time should be 5 minutes by default.
+ */
+const CACHE_TIME = 1000 * 60 * 5;
+
+/**
+ * Create a new default LRU cache with a maximum age of 5 minutes.
+ */
+const defaultCache = new LRUCache({ maxAge: CACHE_TIME });
+
+/**
+ * Generate a new delay for the next request based on the exponential backoff method.
+ *
+ * @param {number} retryNumber The current retry.
+ * @param {object} error The error object.
+ * @returns A new delay in ms for the next request.
+ */
+function customExponentialDelay(retryNumber = 0, error) {
+  const delay = Math.pow(2, retryNumber) * 100;
+  const randomSum = delay * 0.2 * Math.random();
+  return delay + randomSum;
+}
+
+function isPloneError(error) {
+  /**
+   * Those errors can occur when clients close the connection.
+   *
+   * This usually means that Plone is under heavy load.
+   */
+  const ploneErrors = ['ECONNABORTED', 'ECONNRESET', 'EPIPE', 'ECANCELED'];
+  if (error?.code && ploneErrors.includes(error.code)) {
+    return true;
+  }
+
+  /**
+   * Use the default `isNetworkOrIdempotentRequestError` callback.
+   *
+   * This one retries if it is a network error or a 5xx error on an
+   * idempotent request (GET, HEAD, OPTIONS, PUT or DELETE).
+   */
+  return axiosRetry.isNetworkOrIdempotentRequestError(error);
+}
+
+/**
+ * Default options for the PloneClient.
+ */
+const defaultOptions = {
+  // Is the caching adapter enabled?
+  enableCaching: false,
+  // Is the retry plugin enabled?
+  enableRetry: true,
+  // Additional axios instance options.
+  axiosOptions: {},
+};
 
 /**
  * The Plone JS client.
@@ -12,6 +73,10 @@ class PloneClient {
    * @param {Array} options Additional options to configure Axios.
    */
   constructor(url, options = {}) {
+    const clientOptions = {
+      ...defaultOptions,
+      ...options,
+    };
     this.baseURL = url;
 
     // Create a new axios http client.
@@ -23,8 +88,37 @@ class PloneClient {
         'Content-Type': 'application/json',
       },
       timeout: 20000,
-      ...options,
+      // The adapters will always be available.
+      // You can configure if the caching adapter is enabled by default
+      // using the `enableCaching` option.
+      adapter: throttleAdapterEnhancer(
+        cacheAdapterEnhancer(axios.defaults.adapter, {
+          enabledByDefault: clientOptions.enableCaching,
+          defaultCache,
+        })
+      ),
+      // Pass additional options for the axios instance.
+      ...clientOptions.axiosOptions,
     });
+
+    /**
+     * Activate axios-retry plugin if requested.
+     *
+     * We use a custom retry condition callback which handles Plone errors and
+     * falls back to the default callback.
+     *
+     * We also use the `exponentialDelay` function for an exponential backoff retry
+     * delay between requests. See
+     * https://developers.google.com/analytics/devguides/reporting/core/v3/errors#backoff
+     * for details.
+     */
+    if (clientOptions?.enableRetry) {
+      axiosRetry(this.$http, {
+        retries: 3,
+        retryCondition: isPloneError,
+        retryDelay: customExponentialDelay,
+      });
+    }
   }
 
   /**
